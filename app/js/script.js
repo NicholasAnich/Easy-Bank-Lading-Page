22014cd905d2c62211125493bc14941c3482112e
lufarius <infinitevolume@gmail.com>
2022-10-25 11:02:30 -0700

    First initial commit, late commit.
    installed Sass
    Created mobile responsive header
    Created hero section

---
+++ app/js/script.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+hamburger.addEventListener("click", () => {
+    console.log("open hamburger");
+
+    if (header.classList.contains("open")) {
+        // Close Hamburger Menu
+        body.classList.remove("noscroll");
+        header.classList.remove("open");
+        fadeElems.forEach((e) => {
+            e.classList.add("fade-out");
+            e.classList.remove("fade-in");
+        });
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    } else {
+        // Open Hamburger Menu
+        body.classList.add("noscroll");
+        header.classList.add("open");
+        fadeElems.forEach((e) => {
+            e.classList.remove("fade-out");
+            e.classList.add("fade-in");
+        });
+        // overlay.classList.remove("fade-out");
+        // overlay.classList.add("fade-in");
+    }
+});
